92dd7b19e4f640145ea12fc2cd419c30eefd0a6f
minji <mjkim856@gmail.com>
2022-06-14 11:44:28 +0900

    프로젝트 생성 및 User.java 생성

---
+++ src/main/java/vo/User.java
@@ -0,0 +1,38 @@
+package vo;
+
+public class User {
+
+	private int no;
+	private String name;
+	private String email;
+	private String tel;
+	
+	public User () {}
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	
+}
